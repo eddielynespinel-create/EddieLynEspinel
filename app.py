d40b4fe08bf4e12f2d1c29ef1da7636cae903758
eddielynespinel-create <eddielynespinel@gmail.com>
2026-03-12 15:05:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ def get_student():
         "grade": 10,
         "section": "Zechariah"
     })
+    
+@app.route('/grade')
+def grade():
+    return jsonify({
+        "subject": "Math",
+        "grade": 95
 
 if __name__ == "__main__":
     app.run()
